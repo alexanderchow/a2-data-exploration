ae0aa9937882a460533192ce0d86bb41f21325b0
chowicus <chowicus@gmail.com>
2017-04-27 02:02:29 -0700

    comments in js file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-
+/*
+ *  A2-data-exploration
+ *  Alex Chow
+ *  Chlorpleth map to visualize county level US census data.
+ *
+ *  Can be easily modified by simply adding another variable from the data, and another domain of the given variable
+ */
 
  
 $(function() {
 
+  // class constants for domain of each of the variables
+  // can be easily modified and changed with addition of a new variable
   var DOMAIN_POP = [0, 10000, 25000, 50000, 150000, 500000, 750000, 1000000],
       DOMAIN_MHV = [0, 50000, 100000, 150000, 200000, 250000, 300000, 350000],
       DOMAIN_MHI = [0, 25000, 35000, 45000, 55000, 65000, 75000, 85000];
 
+  // set the value of the first color domain
   var colorDomain = DOMAIN_POP;
 
   console.log("hi");
@@ -38,17 +47,17 @@ $(function() {
       .attr('width', drawWidth)
       .attr('height', drawHeight);
 
-  //var unemployment = d3.map();
   var demographics = d3.map();
 
   var path = d3.geoPath();
 
+  // sets the position and domain of the scale
   var x = d3.scaleBand()
       .domain(colorDomain)
       .rangeRound([550, 850]);
 
 
-
+  // sets the color scale based on the data
   var color = d3.scaleThreshold()
       .domain(colorDomain)
       .range(d3.schemeBlues[8]);
@@ -57,6 +66,7 @@ $(function() {
       .attr("class", "key")
       .attr("transform", "translate(0,40)");
 
+  // creates a rectangle to create the scale
   g.selectAll("rect")
     .data(color.range().map(function(d) {
         d = color.invertExtent(d);
@@ -80,16 +90,15 @@ $(function() {
       .text(measure);
 
 
-
+  // reads the csv file and calls the 'ready' function
   d3.queue()
       .defer(d3.json, "https://d3js.org/us-10m.v1.json")
-      //.defer(d3.tsv, "data/unemployment.tsv", function(d) { unemployment.set(d.id, +d.rate); })
       .defer(d3.csv, "data/acs_data.csv", function(d) { demographics.set(d.id2, +d[measure]); })
-      //.defer(d3.csv, "data/acs_data.csv")
       .await(ready);
 
 
 
+  // draws the chloropleth map
   function ready(error, us) {
     if (error) throw error;
 
@@ -100,14 +109,15 @@ $(function() {
       .select(".domain")
       .remove();
 
+    // draws the county objects
     svg.append("g")
         .attr("class", "counties")
       .selectAll("path")
       .data(topojson.feature(us, us.objects.counties).features)
       .enter().append("path")
         .attr("fill", function(d) { return color(d[measure] = demographics.get(d.id)); })
-        //.attr("fill", "blue")
         .attr("d", path)
+        // hover commands
         .on("mouseover", function() {
             d3.select(this).style("fill", "red");
         })
@@ -115,7 +125,6 @@ $(function() {
             d3.select(this).style("fill", function(d) { return color(d[measure] = demographics.get(d.id)); });
         })
       .append("title")
-        //.text(function(d) { return d.rate + "%"; });
         .text(function(d) { return measure + ":  " + d[measure];})
 
 
@@ -127,6 +136,7 @@ $(function() {
 
 
 
+  // function for calling the draw function
   function setColorDomain(value) {
     if (value == 'population') {
       colorDomain = DOMAIN_POP;
@@ -139,6 +149,7 @@ $(function() {
   }
 
 
+  // if the variable is changed, redraw the map
   $("input").on('change', function() {
       // Set your measure variable to the value (which is used in the draw funciton)
       measure = $(this).val();
